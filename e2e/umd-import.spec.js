63082725f0e69261a89dd4c489892ce43471bf53
Evgeny Barabanov <meltedspark@users.noreply.github.com>
2018-03-09 19:39:32 +0200

    Fix node environment (#8)
    * test: add tests to address the issue #7
    
    * fix(package.json): get back all kinds of builds
    
    * test: add import test for minified version
    
    * 1.0.5
    
    * fix(webpack): make globalObject: 'this' until this feature is done: https://github.com/webpack/webpack/issues/6525

---
+++ e2e/umd-import.spec.js
@@ -0,0 +1,16 @@
+const {cold, hot, time} = require('../umd/jest-marbles');
+
+/**
+ * Created by Evgeny Barabanov on 05/03/2018.
+ */
+describe('Imports test', () => {
+
+    it('All the function should exist', () => {
+        const c = cold('a|');
+        const h = hot('a|');
+        const t = time('a|');
+        expect(c).not.toBeNull();
+        expect(h).not.toBeNull();
+        expect(t).not.toBeNull();
+    });
+});
