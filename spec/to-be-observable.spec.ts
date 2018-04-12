98d6e03c757b7289423e7cdc48408d2acd51ea36
Evgeny Barabanov <meltedspark@gmail.com>
2018-04-12 12:47:57 +0300

    feat(output): add subscriptions marblizing, add custom jest matchers that use marblizer, refactor marblizer

--- spec/to-be-observable.spec.ts
+++ spec/to-be-observable.spec.ts
@@ -12,9 +12,9 @@ describe('toBeObservable matcher test', () => {
     });
 
     it('Should merge two hot observables and start emitting from the subscription point', () => {
-        const e1 = hot('----a--^--b-------c--|', {a: 0});
-        const e2 = hot(  '---d-^--e---------f-----|', {a: 0});
-        const expected = cold('---(be)----c-f-----|', {a: 0});
+        const e1 = hot('----a--^--b-------c--|', );
+        const e2 = hot(  '---d-^--e---------f-----|', );
+        const expected = cold('---(be)----c-f-----|', );
 
         expect(e1.pipe(merge(e2))).toBeObservable(expected);
     });
--- src/marblizer.ts
+++ src/marblizer.ts
@@ -1,45 +1,37 @@
 import { TestMessage } from 'rxjs/testing/TestMessage';
+import { SubscriptionLog } from 'rxjs/testing/SubscriptionLog';
+import { MarblesGlossary } from './marbles-glossary';
+import { NotificationKindChars } from './notification-kind';
 
 const frameStep = 10;
 
 export class Marblizer {
-  public marblize(messages: TestMessage[]): string {
+  public static marblize(messages: TestMessage[]): string {
     let openGroup = false;
     let previousMessage = { frame: -frameStep };
     let marble = '';
     let previousFrame = 0;
     for (const message of messages) {
-      //Same notification group
+      // Same notification group
       if (previousMessage.frame === message.frame) {
         if (!openGroup) {
           const lastChar = marble.charAt(marble.length - 1);
-          marble = marble.slice(0, -1) + '(' + lastChar;
+          marble = marble.slice(0, -1) + MarblesGlossary.GroupStart + lastChar;
           openGroup = true;
           previousFrame += frameStep;
         }
       } else {
-        //Different notifications groups
+        // Different notifications groups
         if (openGroup) {
-          //Close the group if open
-          marble += ')';
+          // Close the group if open
+          marble += MarblesGlossary.GroupEnd;
           previousFrame += frameStep;
           openGroup = false;
         }
       }
-      let nextChar;
-      switch (message.notification.kind) {
-        case 'N':
-          nextChar = message.notification.value;
-          break;
-        case 'C':
-          nextChar = '|';
-          break;
-        case 'E':
-          nextChar = '#';
-          break;
-        default:
-          throw Error('Unsupported notification kind');
-      }
+      const nextChar = NotificationKindChars[message.notification.kind] || message.notification.value;
+      if (nextChar === undefined) throw Error('Unsupported notification kind');
+
       const frames = (message.frame - previousFrame) / frameStep;
       if (frames > 0) {
         marble += '-'.repeat(frames);
@@ -52,5 +44,15 @@ export class Marblizer {
     }
 
     return marble;
+  }
+
+  public static marblizeSubscriptions(logs: SubscriptionLog[]): string[] {
+    return logs.map(
+      log =>
+        MarblesGlossary.TimeFrame.repeat(log.subscribedFrame / frameStep) +
+        MarblesGlossary.Subscription +
+        MarblesGlossary.TimeFrame.repeat((log.unsubscribedFrame - log.subscribedFrame) / frameStep - 1) +
+        MarblesGlossary.Unsubscription
+    );
   }
 }
