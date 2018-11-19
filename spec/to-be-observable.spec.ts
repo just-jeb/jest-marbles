50e1e2f18e536e2e944c0d4e750ec9d203953b63
Wouter Spaak <w.spaak@gmail.com>
2018-11-19 12:47:57 +0100

    fix(marblizer): Marblizer now diffs the distance between events based on their actual distance apart.
    Fixes #75.

--- spec/to-be-observable.spec.ts
+++ spec/to-be-observable.spec.ts
@@ -1,38 +1,47 @@
-import {cold, hot, Scheduler, time} from '../index';
-import {concat, merge, mapTo} from 'rxjs/operators';
-import {timer} from 'rxjs';
+import { merge, timer } from 'rxjs';
+import { concat, mapTo } from 'rxjs/operators';
+import { cold, hot, Scheduler, time } from '../index';
 
 describe('toBeObservable matcher test', () => {
-    it('Should concatenate two cold observables into single cold observable', () => {
+  it('Should concatenate two cold observables into single cold observable', () => {
         const a$ = cold('-a-|', {a: 0});
         const b$ = cold('-b-|', {b: 1});
         const expected = cold('-a--b-|', {a: 0, b:1});
 
-        expect(a$.pipe(concat(b$))).toBeObservable(expected);
-    });
+    expect(a$.pipe(concat(b$))).toBeObservable(expected);
+  });
 
-    it('Should work for value objects', () => {
+  it('Should work for value objects', () => {
         const valueObject = {foo: 'bar'};
         const a$ = cold('-a-|', {a: valueObject});
         const expected = cold('-a-|', {a: valueObject});
 
-        expect(a$).toBeObservable(expected);
-    });
+    expect(a$).toBeObservable(expected);
+  });
 
-    it('Should merge two hot observables and start emitting from the subscription point', () => {
+  it('should work for multi-character literals', () => {
+    const falses$ = cold('--a-----b-----|');
+    const trues$ = cold('-----a-----b--|');
+    const expected = cold('--f--t--f--t--|', { t: true, f: false });
+    const mapped = merge(falses$.pipe(mapTo(false)), trues$.pipe(mapTo(true)));
+
+    expect(mapped).toBeObservable(expected);
+  });
+
+  it('Should merge two hot observables and start emitting from the subscription point', () => {
         const e1 = hot('----a--^--b-------c--|', );
         const e2 = hot(  '---d-^--e---------f-----|', );
         const expected = cold('---(be)----c-f-----|', );
 
-        expect(e1.pipe(merge(e2))).toBeObservable(expected);
-    });
+    expect(merge(e1, e2)).toBeObservable(expected);
+  });
 
-    it('Should delay the emission by provided timeout with provided scheduler', () => {
-        const delay = time('-----d|');
-        const provided = timer(delay, Scheduler.get()).pipe(mapTo(0));
+  it('Should delay the emission by provided timeout with provided scheduler', () => {
+    const delay = time('-----d|');
+    const provided = timer(delay, Scheduler.get()).pipe(mapTo(0));
         const expected = hot('------(d|)', {d: 0});
 
-        expect(provided).toBeObservable(expected);
-    });
+    expect(provided).toBeObservable(expected);
+  });
 
 });
--- src/notification-event.ts
+++ src/notification-event.ts
@@ -4,4 +4,17 @@ export class NotificationEvent {
   get end(): number {
     return this.start + this.marbles.length;
   }
+
+  /**
+   * Returns the difference (in empty time frames) between a NotificationEvent
+   * and the previous NotificationEvent.
+   *
+   * @param prev previous event.
+   */
+  diff(prev?: NotificationEvent) {
+    if (prev) {
+      return this.start - prev.start;
+    }
+    return this.start;
+  }
 }
