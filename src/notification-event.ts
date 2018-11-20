c0353f5dffbcd5ac52dda77b0645c4132eb1399c
Wouter Spaak <w.spaak@gmail.com>
2018-11-20 13:02:56 +0100

    refactor(event): removes diff method

--- src/notification-event.ts
+++ src/notification-event.ts
@@ -4,17 +4,4 @@ export class NotificationEvent {
   get end(): number {
     return this.start + this.marbles.length;
   }
-
-  /**
-   * Returns the difference (in empty time frames) between a NotificationEvent
-   * and the previous NotificationEvent.
-   *
-   * @param prev previous event.
-   */
-  diff(prev?: NotificationEvent) {
-    if (prev) {
-      return this.start - prev.start;
-    }
-    return this.start;
-  }
 }
