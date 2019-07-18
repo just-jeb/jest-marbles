0e76f0bd1a7e200db913d8a650b9d83f079d8d06
JeB <meltedspark@gmail.com>
2019-07-18 14:00:52 +0300

    chore: use literal instead of enum

--- src/notification-kind.ts
+++ src/notification-kind.ts
@@ -1,10 +1,11 @@
 import { MarblesGlossary } from './marbles-glossary';
+import { Notification } from 'rxjs';
 export const ValueLiteral = {};
 
-export type NotificationKind = 'N' | 'C' | 'E';
+export type NotificationKind = Notification<any>['kind'];
 
 export const NotificationKindChars: { [key in NotificationKind]: any } = {
-  ['N']: ValueLiteral,
-  ['C']: MarblesGlossary.Completion,
-  ['E']: MarblesGlossary.Error,
+  N: ValueLiteral,
+  C: MarblesGlossary.Completion,
+  E: MarblesGlossary.Error,
 };
