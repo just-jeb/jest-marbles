9b33306d6a07928bc6da8fa818bba0c4d1085dee
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2023-12-03 17:26:25 +0100

    fix: typing for globally imported expect (#409)

---
+++ spec/typing-work-for-global-import.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from '@jest/globals'
+import { cold } from "../index";
+
+describe('', () => {
+  it('Should have typing for expect imported from globals', () => {
+    const a$ = cold("a", { a: 3 })
+
+    expect(a$).toBeObservable(a$);
+  })
+})
